bf8da1a1deb027567f7a587bfd3d32c4f00eb82f
Jonathan Célio <jonathan.clio@hotmail.com>
2020-04-19 01:44:53 -0300

    test: correction on synthax

--- src/utils/checkTokenValid.test.js
+++ src/utils/checkTokenValid.test.js
@@ -3,12 +3,12 @@ import generateTestToken from './generateTestToken';
 
 
 describe('checkTokenValid()', () => {
-  test('should return false for old token', async () => {
+  it('should return false for old token', async () => {
     const token = generateTestToken('2020-03-01');
     const isTokenValid = checkTokenValid(token);
     expect(isTokenValid).toBe(false);
   });
-  test('should return true for new token', async () => {
+  it('should return true for new token', async () => {
     const myDate = new Date();
     myDate.setDate(myDate.getDate() + 1);
     const token = generateTestToken(myDate.toString());
--- src/utils/generateTestToken.test.js
+++ src/utils/generateTestToken.test.js
@@ -1,7 +1,7 @@
 import generateTestToken from './generateTestToken';
 
 describe('generateTestToken()', () => {
-  test('should generate token properly', async () => {
+  it('should generate token properly', async () => {
     const token = generateTestToken('2020-03-01');
     expect(token).toBe('abc.eyJleHAiOjE1ODMwMjA4MDB9');
   });
