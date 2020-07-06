e614793390c98b956c1fd98aedf1e7fc01de77ab
Jonathan Célio <jonathan.clio@hotmail.com>
2020-07-06 16:57:45 -0300

    test: update tests file

--- tests/MelhorEnvio/calculateShipment.test.ts
+++ tests/MelhorEnvio/calculateShipment.test.ts
@@ -1,7 +1,7 @@
 import MelhorEnvio from '../../src';
 
 test('call calculateShipment and check response', async () => {
-  const me = new MelhorEnvio(process.env.API_ME_TOKEN, true, 20000);
+  const me = new MelhorEnvio(String(process.env.API_ME_TOKEN), true, 20000);
   const response = await me.calculateShipment(
     '12608220',
     '68637000',
@@ -9,13 +9,12 @@ test('call calculateShipment and check response', async () => {
       height: 11,
       length: 20,
       width: 30,
-      weight: 0.14,
+      weight: 0.14
     },
-    [],
-    [],
+    null,
     false,
     false,
-    200,
+    200
   );
   // console.log(response);
   expect(response).toBeTruthy();
--- tests/MelhorEnvio/getShipmentServices.test.ts
+++ tests/MelhorEnvio/getShipmentServices.test.ts
@@ -1,7 +1,7 @@
 import MelhorEnvio from '../../src';
 
 test('call getShipmentServices and check response', async () => {
-  const me = new MelhorEnvio(process.env.API_ME_TOKEN, true, 20000);
+  const me = new MelhorEnvio(String(process.env.API_ME_TOKEN), true, 20000);
   const response = await me.getShipmentServices();
   // console.log(response);
   expect(response).toBeTruthy();
